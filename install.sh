825d4deb1e5a6afa1fc9909114345898f0674488
d0ntay <dachich10@gmail.com>
2024-05-28 18:41:18 -0400

    install script now works

--- install.sh
+++ install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-git clone https://github.com/libcpr/cpr.git .
-cd cpr && mkdir build && cd build
-cmake .. -DCPR_USE_SYSTEM_CURL=ON
-cmake --build . --parallel
-sudo cmake --install .
-cd ..
 cd url-finder && mkdir build && cd build
 cmake ..
 make
+cp -r url-finder ../../url-finder-run
